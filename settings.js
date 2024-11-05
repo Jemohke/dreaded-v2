57e094e6419a8566ca09f06af2c95c88096186ce
Jemohke <151847767+Jemohke@users.noreply.github.com>
2024-11-05 19:43:30 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ const sessionName = "session";
 const presence = process.env.WA_PRESENCE || 'online';
 
 const antitag = process.env.ANTITAG || 'true';
-const antidelete = process.env.ANTIDELETE || 'true';
-const autoview = process.env.AUTOVIEW_STATUS || 'true';
-const autolike = process.env.AUTOLIKE_STATUS || 'true';
+const antidelete = process.env.ANTIDELETE || 'false';
+const autoview = process.env.AUTOVIEW_STATUS || 'false';
+const autolike = process.env.AUTOLIKE_STATUS || 'false';
 const autoread = process.env.AUTOREAD || 'true';
 const autobio = process.env.AUTOBIO || 'false';
 
